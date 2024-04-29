56cad9e468694b5c769f5a283a9f48703d8f9fda
dkwelcher <dkwelcher@gmail.com>
2024-04-29 12:48:00 -0400

    Added component for linking user to dashboard

---
+++ src/components/landing/shared/Link.jsx
@@ -0,0 +1,13 @@
+function Link() {
+  return (
+    <>
+      <div className="font-primary">
+        <a className="p-4 rounded-2xl text-base text-sky-100 bg-sky-900 cursor-pointer hover:text-sky-50 hover:bg-sky-700">
+          Go to Dashboard
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default Link;
