a98cdb5ad94574c779901b9e2798fe9155fa016f
dkwelcher <dkwelcher@gmail.com>
2024-04-29 11:46:22 -0400

    Added initial route to landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/landing/LandingPage";
+
 function App() {
   return (
     <>
-      <div>Flight Planner</div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
