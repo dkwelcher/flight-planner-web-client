f90cb43bdb3594170e9fe3ea5d24cd298a72a2b7
dkwelcher <dkwelcher@gmail.com>
2024-04-29 12:48:39 -0400

    Added app-shared component for Logo including image & app name

---
+++ src/components/shared/Logo.jsx
@@ -0,0 +1,18 @@
+import LogoImage from "../../assets/flight-planner-logo.png";
+
+function Logo({ imageSize, textSize }) {
+  return (
+    <>
+      <div className="flex items-center font-secondary">
+        <img
+          className={`${imageSize}`}
+          src={LogoImage}
+          alt="Flight Planner log of an outline of a commercial jet"
+        />
+        <h2 className={`${textSize}`}>Flight Planner</h2>
+      </div>
+    </>
+  );
+}
+
+export default Logo;
