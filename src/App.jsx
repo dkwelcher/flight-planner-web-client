882a60bfe1e79918d14690b697181b3b715fbbb7
dkwelcher <dkwelcher@gmail.com>
2024-05-06 16:08:00 -0400

    Added dashboard route with manage aircrafts link

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/landing/LandingPage";
+import Layout from "./components/dashboard/Layout";
+import ManageAircraft from "./components/views/aircraft/ManageAircraft";
 
-function App() {
+export default function App() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Layout />}>
+            <Route path="aircraft/manage" element={<ManageAircraft />}></Route>
+          </Route>
         </Routes>
       </Router>
     </>
   );
 }
-
-export default App;
