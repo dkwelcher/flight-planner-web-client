31d46ea1ad9f2980ed3d3707ff2e3e8bb42f428c
dkwelcher <dkwelcher@gmail.com>
2024-05-06 16:09:43 -0400

    Moved export default statement inline with function signature

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -1,15 +1,13 @@
 import Logo from "../shared/Logo";
 import Link from "./shared/Link";
 
-function LandingPage() {
+export default function LandingPage() {
   return (
     <>
-      <div className="h-screen flex flex-col justify-center items-center gap-y-6 bg-sky-100">
+      <div className="h-screen flex flex-col justify-center items-center gap-y-6 bg-sky-50">
         <Logo imageSize="size-16" textSize="text-4xl" />
         <Link />
       </div>
     </>
   );
 }
-
-export default LandingPage;
