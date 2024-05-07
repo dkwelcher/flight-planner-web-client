4271602c342989d208673e60031925201055146a
dkwelcher <dkwelcher@gmail.com>
2024-05-07 11:42:13 -0400

    Added logout functionality & fixed pathname typo that renders active tab state

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Logo from "../shared/Logo";
 import {
   DASHBOARD_SIDEBAR_BOTTOM_LINKS,
@@ -8,6 +8,12 @@ import classNames from "classnames";
 import { HiOutlineLogout } from "react-icons/hi";
 
 export default function Sidebar({ screenWidth }) {
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    navigate("/");
+  }
+
   return (
     <>
       <div
@@ -31,7 +37,12 @@ export default function Sidebar({ screenWidth }) {
           {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((link) => (
             <SidebarLink key={link.key} link={link} />
           ))}
-          <div className="flex items-center gap-2 font-light px-3 py-2 text-red-500 rounded-sm text-base cursor-pointer hover:bg-sky-700 hover:no-underline active:bg-sky-600">
+          <div
+            className="flex items-center gap-2 font-light px-3 py-2 text-red-500 rounded-sm text-base cursor-pointer hover:bg-sky-700 hover:no-underline active:bg-sky-600"
+            onClick={() => {
+              handleLogout();
+            }}
+          >
             <span className="text-2xl">{<HiOutlineLogout />}</span>
             Logout
           </div>
@@ -42,13 +53,13 @@ export default function Sidebar({ screenWidth }) {
 }
 
 function SidebarLink({ link }) {
-  const { pathName } = useLocation();
+  const { pathname } = useLocation();
 
   return (
     <Link
       to={link.path}
       className={classNames(
-        pathName === link.path ? "bg-sky-800 text-sky-300" : "text-sky-400",
+        pathname === link.path ? "bg-sky-800 text-sky-300" : "text-sky-400",
         "flex items-center gap-2 font-light px-3 py-2 hover:bg-sky-700 hover:no-underline active:bg-sky-600 rounded-sm text-base"
       )}
     >
