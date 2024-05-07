bd9f3d0c19268b1642071c765bb055fab3882655
dkwelcher <dkwelcher@gmail.com>
2024-05-07 11:44:17 -0400

    Added navigation functionality using userNavigate

--- src/components/landing/shared/Link.jsx
+++ src/components/landing/shared/Link.jsx
@@ -1,8 +1,21 @@
-export default function Link() {
+import { useNavigate } from "react-router-dom";
+
+export default function Link({ navigationPath }) {
+  const navigate = useNavigate();
+
+  function handleNavigation() {
+    navigate(navigationPath);
+  }
+
   return (
     <>
       <div className="font-primary">
-        <a className="p-4 rounded-2xl text-base text-sky-100 bg-sky-900 cursor-pointer hover:text-sky-50 hover:bg-sky-700">
+        <a
+          className="p-4 rounded-2xl text-base text-sky-100 bg-sky-900 cursor-pointer hover:text-sky-50 hover:bg-sky-700"
+          onClick={() => {
+            handleNavigation();
+          }}
+        >
           Go to Dashboard
         </a>
       </div>
