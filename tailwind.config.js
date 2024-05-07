d0340a9920cf5b71fe23c20a3e663c0b4f1138b5
dkwelcher <dkwelcher@gmail.com>
2024-05-07 11:39:09 -0400

    Added background Image & textShadow config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require("tailwindcss/plugin");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "manage-aircraft-intro-desktop":
+          "url('/src/assets/prop-countryside.png')",
+      },
       fontFamily: {
         primary: ["Prompt", "sans-serif"],
         secondary: ["Play", "sans-serif"],
       },
+      textShadow: {
+        default:
+          "-1px -1px 0 rgb(64 64 64), 1px -1px 0 rgb(64 64 64), -1px 1px 0 #000, 1px 1px 0 rgb(64 64 64)",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities, theme }) {
+      const newUtilities = {
+        ".text-shadow": {
+          textShadow: theme("textShadow.default"),
+        },
+      };
+      addUtilities(newUtilities);
+    }),
+  ],
 };
