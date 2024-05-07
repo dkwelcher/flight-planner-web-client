3940a11a21a97109811c9d11c85946b56ce82cc1
dkwelcher <dkwelcher@gmail.com>
2024-05-07 11:39:46 -0400

    Added dashboard route & screen resizing function

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/landing/LandingPage";
 import Layout from "./components/dashboard/Layout";
+import Home from "./components/views/home/Home";
 import ManageAircraft from "./components/views/aircraft/ManageAircraft";
 
 export default function App() {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/dashboard" element={<Layout />}>
-            <Route path="aircraft/manage" element={<ManageAircraft />}></Route>
+          <Route
+            path="/dashboard"
+            element={<Layout screenWidth={screenWidth} />}
+          >
+            <Route index element={<Home />} />
+            <Route path="aircraft/manage" element={<ManageAircraft />} />
           </Route>
         </Routes>
       </Router>
