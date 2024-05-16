e6f8a68a6f0940e512d413cfcf16b8a1ac7b4db8
dkwelcher <dkwelcher@gmail.com>
2024-05-16 10:11:53 -0400

    Simplified rendered data. Accepts the entire aircraft object. Assigns data-key to each button accord. to object index in array
    The data-key for each button will allow for easy access to the chosen aircraft object in the aircraft array

--- src/components/views/aircraft/shared/AircraftCard.jsx
+++ src/components/views/aircraft/shared/AircraftCard.jsx
@@ -1,80 +1,40 @@
-import Button from "../../shared/Button";
+import Button from "../../../shared/Button";
 
-export default function AircraftCard({
-  aircraftId,
-  aircraftMake,
-  aircraftModel,
-  aircraftType,
-  fuelType,
-  fuelTankSize,
-  fuelBurnRate,
-  airspeed,
-  minimumRunwayLengthRequired,
-  index,
-}) {
-  const headingContainerStyle = "p-1 border border-sky-600 rounded-md";
-  const headingStyle = "font-semibold border-b border-sky-400";
+export default function AircraftCard({ aircraft, index }) {
   const buttonContainerStyle = "p-4 grid grid-cols-2 gap-x-2";
 
   return (
     <div
-      className={`mt-4 p-2 font-primary rounded-md ${
-        index % 2 == 0 ? "bg-sky-300 text-sky-900" : "bg-sky-200 text-sky-800"
+      className={`mt-4 p-2 flex flex-col justify-center font-primary rounded-md shadow-md ${
+        index % 2 == 0
+          ? "bg-sky-300 text-sky-900 shadow-sky-600"
+          : "bg-sky-200 text-sky-800 shadow-sky-500"
       }`}
-      key={aircraftId}
+      key={aircraft.aircraftId}
     >
       <div className="pb-2 font-secondary font-bold text-3xl text-sky-950">
-        <h4>
-          {aircraftMake} {aircraftModel}
-        </h4>
-      </div>
-      <div className="grid grid-cols-2 2xl:grid-cols-3 gap-1">
-        <div className={headingContainerStyle}>
-          <p className={headingStyle}>Make:</p>
-          <p>{aircraftMake}</p>
-        </div>
-        <div className={headingContainerStyle}>
-          <p className={headingStyle}>Model:</p>
-          <p>{aircraftModel}</p>
-        </div>
-        <div className={headingContainerStyle}>
-          <p className={headingStyle}>Aircraft Type:</p>
-          <p>{aircraftType}</p>
-        </div>
-        <div className={headingContainerStyle}>
-          <p className={headingStyle}>Fuel Type:</p>
-          <p>{fuelType}</p>
-        </div>
-        <div className={headingContainerStyle}>
-          <p className={headingStyle}>Fuel Tank Size:</p>
-          <p>{fuelTankSize}</p>
-        </div>
-        <div className={headingContainerStyle}>
-          <p className={headingStyle}>Fuel Burn Rate:</p>
-          <p>{fuelBurnRate}</p>
-        </div>
-        <div className={headingContainerStyle}>
-          <p className={headingStyle}>Cruise Airspeed:</p>
-          <p>{airspeed}</p>
-        </div>
-        <div className={headingContainerStyle}>
-          <p className={headingStyle}>Minimum Runway Length Required:</p>
-          <p>{minimumRunwayLengthRequired}</p>
+        <div className="flex justify-between">
+          <h2>
+            {aircraft.aircraftMake} {aircraft.aircraftModel}
+          </h2>
+          <p className="font-primary font-normal text-base">
+            {aircraft.aircraftType}
+          </p>
         </div>
       </div>
       <div className={buttonContainerStyle}>
         <Button
-          uniqueStyles={"lg:justify-self-end"}
           buttonText={"Edit"}
+          data-key={index}
           handleUserAction={() => {
-            console.log(aircraftId);
+            console.log(aircraft.aircraftId);
           }}
         />
         <Button
-          uniqueStyles={"lg:justify-self-start"}
           buttonText={"Delete"}
+          data-key={index}
           handleUserAction={() => {
-            console.log(aircraftId);
+            console.log(aircraft.aircraftId);
           }}
         />
       </div>
